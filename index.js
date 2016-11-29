6b30e6d225027bb87ba2be61a3fc42e0aee763a4
Kostis Maninakis <maninak@plirono.com>
2016-11-29 23:27:26 +0200

    feat(webserver): allow app to fetch resources from external servers

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('www', { maxAge: oneHour })); // Client-side file caching
 
+/* Allow fetching of additional external resouces. Warning: Possible security risk */
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
