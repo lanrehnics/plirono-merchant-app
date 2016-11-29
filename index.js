8fb4a7d641c055cd83cb4904298ff7de28561bf9
Kostis Maninakis <maninak@plirono.com>
2016-11-29 23:12:44 +0200

    feat(webserver): add client-side file caching

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+var oneHour     = 3600000;  // 3600000msec == 1hour
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('www')); 
+app.use(express.static('www', { maxAge: oneHour })); // Client-side file caching
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
