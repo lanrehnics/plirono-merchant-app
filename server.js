5aa7adc06118856622c113cfc91326eb8764ddab
Kostis Maninakis <maninak@plirono.com>
2016-12-05 12:26:29 +0200

    style(webserver): clean-up

--- server.js
+++ server.js
@@ -7,11 +7,10 @@ const oneHour       = 3600000;    // 3600000msec == 1hour
 const oneMinute     = 60000;      // 60000 == 1 minute
 
 server.use(compression({ level: 5 }));
-server.use(express.static('www', { maxAge: oneHour }));       // Client-side file caching
+server.use(express.static('www', { maxAge: oneHour })); // Client-side file caching
 
 server.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-
 
 server.listen(8080);
