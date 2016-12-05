131552df75c273f09dc27afca1e8c4d74b92819a
Kostis Maninakis <maninak@plirono.com>
2016-12-05 12:25:48 +0200

    feat(webserver): no loger allows cross-site requests

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ const oneMinute     = 60000;      // 60000 == 1 minute
 server.use(compression({ level: 5 }));
 server.use(express.static('www', { maxAge: oneHour }));       // Client-side file caching
 
-/* Allow fetching of additional external resouces. Warning: Possible security risk! */
-server.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 server.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
