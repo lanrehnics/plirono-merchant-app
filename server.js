df63cd9ad9dcae717c4c78d1765134a7371cc16b
Kostis Maninakis <maninak@plirono.com>
2016-12-05 12:19:59 +0200

    perf(webserver): remove node module `body-parser` from project dependencies

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express       = require('express');
 const compression   = require('compression');
-const bodyParser    = require('body-parser');
 
 
 const server        = express();
@@ -8,8 +7,6 @@ const oneHour       = 3600000;    // 3600000msec == 1hour
 const oneMinute     = 60000;      // 60000 == 1 minute
 
 server.use(compression({ level: 5 }));
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: true }));
 server.use(express.static('www', { maxAge: oneHour }));       // Client-side file caching
 
 /* Allow fetching of additional external resouces. Warning: Possible security risk! */
