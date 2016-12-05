f7c73fba1dc3f68bab17e4fbd2b16b5abc8a6e11
Kostis Maninakis <maninak@plirono.com>
2016-12-05 10:20:35 +0200

    refactor(webserver): `app` -> `server`

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const path 					= require('path');
 const bodyParser 		= require('body-parser');
 
 
-const app 					= express();
-const oneHour 			= 3600000; // 3600000msec == 1hour
+const server 				= express();
+const oneHour 			= 3600000; 		// 3600000msec == 1hour
 const oneMinute			= 60000;			// 60000 == 1 minute
 
-app.use(compression({ level: 5 }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('www', { maxAge: oneHour })); // Client-side file caching
-app.use(cache({rules: [{regex: /.*/, ttl: oneMinute }]})); // Server-side file caching
+server.use(cache({rules: [{regex: /.*/, ttl: oneMinute }]})); // Server-side file caching
+server.use(compression({ level: 5 }));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(express.static('www', { maxAge: oneHour })); // Client-side file caching
 
 /* Allow fetching of additional external resouces. Warning: Possible security risk! */
-app.use(function(req, res, next) {
+server.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-app.get('/', function(req, res) {
+server.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 
-app.listen(8080);
+server.listen(8080);
