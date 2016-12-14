6cfaf9b88d7eda2584bca343ca525a3322a4d044
Kostis Maninakis <maninak@plirono.com>
2016-12-14 17:02:48 +0200

    feat(favicon): app now has favicon and PWA configuration for any platform

---
+++ config/copy.config.js
@@ -0,0 +1,24 @@
+// original source from https://github.com/driftyco/ionic-app-scripts/blob/master/config/copy.config.js
+// in plirono we use this passed as a param when serving and building to ovverride the default files copied 
+
+// this is a custom dictionary to make it easy to extend/override
+// provide a name for an entry, it can be anything such as 'copyAssets' or 'copyFonts'
+// then provide an object with a `src` array of globs and a `dest` string
+module.exports = {
+  copyAssets: {
+    src: ['{{SRC}}/assets/**/*'],
+    dest: '{{WWW}}/assets'
+  },
+  copyIndexContent: {
+    src: ['{{SRC}}/index.html', '{{SRC}}/manifest.json', '{{SRC}}/service-worker.js', '{{SRC}}/browserconfig.xml'],
+    dest: '{{WWW}}'
+  },
+  copyFonts: {
+    src: ['{{ROOT}}/node_modules/ionicons/dist/fonts/**/*', '{{ROOT}}/node_modules/ionic-angular/fonts/**/*'],
+    dest: '{{WWW}}/assets/fonts'
+  },
+  copyPolyfills: {
+    src: ['{{ROOT}}/node_modules/ionic-angular/polyfills/polyfills.js'],
+    dest: '{{BUILD}}'
+  }
+}
